1adf0bc3bec4a00ddeddb235e107f1d853d707ac
JingyeXia <70971515+Jingye-Xia@users.noreply.github.com>
2021-03-16 15:43:27 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -27,7 +27,7 @@ cat << EOF > /usr/local/etc/xray/config.json
                 "decryption": "none"
             },
             "streamSettings": {
-                "network": "ws"
+                "network": "ws",
                 "wsSettings": {
                     "path": "/xjyv2ray"
                 }
